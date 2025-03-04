e654ec7c044170cbf7bc8e50a93e21c9dce88a5d
douglus <douglus@rexy.tech>
2025-03-04 23:47:54 +0700

    perf: optimize dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import NavBar from './components/NavBar'
-import { Home, About, Projects, Experience, Skills } from './pages'
+const Home = lazy(() => import("./pages/Home"))
+const About = lazy(() => import("./pages/About"))
+const Projects = lazy(() => import("./pages/Projects"))
+const Experience = lazy(() => import("./pages/Experience"))
+const Skills = lazy(() => import("./pages/Skills"))
 
 const App = () => {
   return (
-    <main className='bg-slate-300/20 min-h-[100vh]'>
-      <Router>
-        <NavBar />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/projects' element={<Projects />} />
-          <Route path='/experience' element={<Experience />} />
-          <Route path='/skills' element={<Skills />} />
-        </Routes>
-      </Router>
-    </main>
+    <Suspense fallback={<div>Loading...</div>}>
+      <main className='bg-slate-300/20 min-h-[100vh]'>
+        <Router>
+          <NavBar />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/projects' element={<Projects />} />
+            <Route path='/experience' element={<Experience />} />
+            <Route path='/skills' element={<Skills />} />
+          </Routes>
+        </Router>
+      </main>
+    </Suspense>
   )
 }
 
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,63 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import { chunkSplitPlugin } from 'vite-plugin-chunk-split';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  assetsInclude: ['**/*.glb']
-})
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    ViteImageOptimizer(),
+    chunkSplitPlugin({
+      strategy: 'single-vendor',
+      customChunk: (args) => {
+        let { file } = args;
+        if (file.startsWith('src/pages/')) {
+          file = file.substring(4);
+          file = file.replace(/\.[^.$]/, '');
+          return false;
+        }
+        return null;
+      },
+    }),
+  ],
+  assetsInclude: ['**/*.glb'],
+  optimizeDeps: {
+    force: true,
+    include: ['three'],
+    esbuildOptions: {
+      loader: {
+        '.ts': 'tsx',
+      },
+    },
+  },
+  build: {
+    chunkSizeWarningLimit: 1600,
+    outDir: 'dist',
+    minify: 'terser',
+    manifest: true,
+    sourcemap: false,
+    reportCompressedSize: true,
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code == 'EVAL') return;
+        warn(warning);
+      },
+      output: {
+        manualChunks(id) {
+          if (id.indexOf('node_modules') !== -1) {
+            const basic = id.toString().split('node_modules/')[1];
+            const sub1 = basic.split('/')[0];
+            if (sub1 !== '.pnpm') {
+              return sub1.toString();
+            }
+            const name2 = basic.split('/')[1];
+            return name2.split('@')[name2[0] === '@' ? 1: 0].toString();
+          }
+        },
+      },
+    },
+  },
+});
