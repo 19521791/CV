90b8ecb62981deccff4f59a681bcea25e8fc8785
douglus <douglus@rexy.tech>
2025-03-31 03:36:53 +0700

    fix: remove experience

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,7 @@
 import Model from './Model'
 import About from './About'
 import Projects from './Projects'
-import Experience from './Experience'
 import Skills from './Skills'
 import CoverLetter from './CoverLetter'
 
-export { Model, About, Projects, Experience, Skills, CoverLetter }
+export { Model, About, Projects, Skills, CoverLetter }
