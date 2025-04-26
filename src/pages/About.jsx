feeccbd9da9bb4ca2488c299045aff05ba44a34c
Nguyen Long <toannguyenvan145@gmail.com>
2025-04-26 15:51:18 +0700

    fix: fix reponsive tab about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,7 +8,7 @@ const Item = ({ src, text }) => {
   return (
     <div className='text-base md:text-lg lg:text-xl flex flex-row items-center justify-start'>
       <img src={src} className='hidden sm:inline-block w-6 h-6 mr-2' />
-      <div className='block sm:hidden w-1 h-1 rounded-full bg-black mr-2'></div>
+      <div className='inline-block sm:hidden w-1 h-1 rounded-full bg-black mr-2'></div>
       <span>{ text }</span>
     </div>
   )
@@ -28,20 +28,20 @@ const About = () => {
   }
 
   return (
-    <main className="mx-auto p-4 sm:p-8 pb-12 !pt-[100px] min-h-[calc(100vh-80px)] max-w-[1500px]">
+    <main className="mx-auto p-4 sm:py-8 sm:px-4 pb-12 !pt-[100px] min-h-[calc(100vh-80px)] max-w-[1500px]">
       <div className='info-container flex flex-col'>
         <div className='flex flex-col xd:flex-row gap-6 sm:gap-10 xd:gap-4 xm:gap-5 lg:gap-6 xl:gap-10  xl:pb-[80px]'>
           <div className='w-full flex justify-center'>
             <img
               src={images['myself']}
               alt='My Self'
-              className='h-[60vh] w-[90vw] sm:h-[600px] sm:w-[600px] md:h-[480px] md:w-[480px] xm:h-[500px] xm:w-[500px] lg:h-[450px] lg:w-[450px]
+              className='h-[60vh] w-[90vw] sm:h-[600px] sm:w-[600px] md:h-[480px] md:w-[480px] xm:h-[500px] xm:w-[500px] lg:h-[550px] lg:w-[450px]
             xl:h-[600px] xl:w-[600px] max-w-none rounded-md object-cover'
               loading='eager'
             />
           </div>
 
-          <div className='xl:pt-5 lg:lex lg:flex-col xl:mb-10'>
+          <div className='xl:pt-5 lg:flex lg:flex-col xl:mb-10'>
             <div className='paragraph lg:mr-[20px]'>
               <h1 className='text-4xl sm:text-5xl xm:text-6xl mb-4 font-medium font-[Sora]'>Who I Am, Briefly</h1>
               <div className='text-base md:text-lg lg:text-xl md:leading-snug lg:leading-normal xl:leading-loose text-slate-600 lg:mb-7 xl:mb-8'>Hi, My name is Nguyen Phi Long. I&apos;m from Dak Lak and work as a backend developer.
@@ -51,14 +51,14 @@ const About = () => {
 
             <div className='hidden lg:block'>
               <div className='info'>
-                <div className='flex flex-row lg:gap-16 xl:gap-20 mt-3'>
+                <div className='flex flex-col gap-5 mt-3'>
                   <div className='info-contact flex flex-col lg:gap-3 xl:gap-4'>
                     <Item src={images['mail']} text={'toannguyenvan145@gmail.com'} />
                     <Item src={images['phone']} text={'+84 393 277 584'} />
                     <Item src={images['location']} text={'Hiep Binh Phuoc, Thu Duc, Ho Chi Minh City'} />
                   </div>
 
-                  <div className='info-contact flex flex-col lg:gap-3 xl:gap-4'>
+                  <div className='info-contact flex flex-row gap-10'>
                     <div>
                       <a href={githubLink}>
                         <Item src={images['github']} text={'Github'} />
@@ -104,7 +104,7 @@ const About = () => {
           </div>
         </div>
 
-        <p className='text-xl pb-1'>Tech Stack</p>
+        <p className='text-xl pb-1 lg:mt-5 xl:mt-0'>Tech Stack</p>
         <div className='info info-contact'>
           <div className='hidden lg:flex flex-row justify-between mx-10 xl:mx-20'>
             <div className='flex flex-col gap-3 rounded-lg lg:border-[1px] lg:border-[#B1A994] py-3 lg:p-3'>
