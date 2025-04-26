e51851a4b8b8cce3322d093254f15815d6aa824e
douglus <douglus@rexy.tech>
2025-04-26 08:04:06 +0700

    fix: fix linter

--- src/components/loading/LoaderProgress.jsx
+++ src/components/loading/LoaderProgress.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useContext, useEffect } from 'react'
 import gsap from 'gsap'
 import { AnimationContext } from '@/contexts/AnimationContext'
